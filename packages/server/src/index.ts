ac8c25392469279bfc52e20e3b3ebd0e4223cd89
Samsoedien <contact@samsoedien.com>
2020-03-25 22:19:21 +0100

    feat(server): create basic express app

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,13 @@
-console.log('hello world')
+import http from 'http'
+
+import app from './api/app'
+import connectWS from './ws'
+
+const PORT = process.env.PORT || 5000
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
+  // Connect to DB
+  connectWS()
+  console.info(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+})
---
+++ packages/server/src/ws.ts
@@ -0,0 +1,5 @@
+import socketio from 'socket.io'
+
+const connectWS = () => console.log('HI im a websocket')
+
+export default connectWS
