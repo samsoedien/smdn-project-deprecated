42e4b463149ae870a7c3d387db9a43ade2039a87
Samsoedien <contact@samsoedien.com>
2020-07-09 23:23:50 +0200

    fix: main stylesheet file imports

--- packages/client/src/App/App.tsx
+++ packages/client/src/App/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 // import logo from '../logo.svg'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import styles from './App.module.scss'
 
 // import { Button } from '@smdn/core'
@@ -12,6 +12,7 @@ const App: React.FC<IAppProps> = () => {
   return (
     <div className={styles['App']}>
       <p className={styles['App__text']}>App</p>
+      {/* <Chat /> */}
     </div>
   )
 }
