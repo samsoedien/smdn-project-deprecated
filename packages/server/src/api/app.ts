4063231cc49046be27c74acfd20bd9544e6c3975
Samsoedien <contact@samsoedien.com>
2020-07-11 00:14:51 +0200

    build: add build scripts for all packages to transpile ts to commonjs

--- packages/server/src/api/app.ts
+++ packages/server/src/api/app.ts
@@ -9,11 +9,11 @@ import rateLimit from 'express-rate-limit'
 import xss from 'xss-clean'
 import cookieparser from 'cookie-parser'
 
-import errorHandler from './middleware/error.middleware'
-import authRoutes from './routes/auth.route'
-import productRoutes from './routes/products.route'
-import userRoutes from './routes/users.route'
-import reviewRoutes from './routes/reviews.route'
+// import authRoutes from './routes/auth.route'
+// import productRoutes from './routes/products.route'
+// import userRoutes from './routes/users.route'
+// import reviewRoutes from './routes/reviews.route'
+// import errorHandler from './middleware/error.middleware'
 
 const app: Application = express()
 
@@ -38,11 +38,11 @@ app.use(limiter)
 
 app.get('/', (req: Request, res: Response) => res.send('Rest API Running.'))
 
-app.use('/api/v1/auth', authRoutes)
-app.use('/api/v1/users', userRoutes)
-app.use('/api/v1/products', productRoutes)
-app.use('/api/v1/reviews', reviewRoutes)
+// app.use('/api/v1/auth', authRoutes)
+// app.use('/api/v1/users', userRoutes)
+// app.use('/api/v1/products', productRoutes)
+// app.use('/api/v1/reviews', reviewRoutes)
 
-app.use(errorHandler)
+// app.use(errorHandler)
 
 export default app
--- packages/server/src/api/middleware/query.middelware.ts
+++ packages/server/src/api/middleware/query.middelware.ts
@@ -1,84 +1,65 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express'
 
 const advancedQuery: any = (model: any, populate: string) => async (req: Request, res: any, next: NextFunction) => {
-  let query
-
-  const reqQuery = { ...req.query }
-
-  const removeFields = ['select', 'sort', 'page', 'limit']
-
-  removeFields.forEach((param) => delete reqQuery[param])
-
-  console.log(reqQuery)
-
-  let queryStr = JSON.stringify(reqQuery)
-
-  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`)
-
-  query = model.find(JSON.parse(queryStr))
-
-  if (req.query.select) {
-    const fields = req.query.select.split(',').join(' ')
-    query.select(fields)
-  }
-
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split('').join(' ')
-    query = query.sort(sortBy)
-  } else {
-    query = query.sort('-createdAt')
-  }
-
-  const page = parseInt(req.query.page, 10) || 1
-  const limit = parseInt(req.query.limit, 10) || 24
-  const startIndex = (page - 1) * limit
-  const endIndex = page * limit
-  const total = await model.countDocuments()
-
-  query = query.skip(startIndex).limit(limit)
-
-  if (populate) {
-    query = query.populate(populate)
-  }
-
-  interface IPagination {
-    next: {}
-    previous: {}
-  }
-
-  const results = await query
-
-  interface IPaginition {
-    prev: any
-    next: any
-  }
-
-  const pagination: IPaginition = {
-    prev: 0,
-    next: 0,
-  }
-
-  if (endIndex < total) {
-    pagination.next = {
-      page: page + 1,
-      limit,
-    }
-  }
-
-  if (startIndex > 0) {
-    pagination.prev = {
-      page: page - 1,
-      limit,
-    }
-  }
-
-  res.advancedQuery = {
-    success: true,
-    count: results.length,
-    pagination,
-    data: results,
-  }
-  next()
+  //   let query
+  //   const reqQuery = { ...req.query }
+  //   const removeFields = ['select', 'sort', 'page', 'limit']
+  //   removeFields.forEach((param) => delete reqQuery[param])
+  //   console.log(reqQuery)
+  //   let queryStr = JSON.stringify(reqQuery)
+  //   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`)
+  //   query = model.find(JSON.parse(queryStr))
+  //   if (req.query.select) {
+  //     const fields = req.query.select.split(',').join(' ')
+  //     query.select(fields)
+  //   }
+  //   if (req.query.sort) {
+  //     const sortBy = req.query.sort.split('').join(' ')
+  //     query = query.sort(sortBy)
+  //   } else {
+  //     query = query.sort('-createdAt')
+  //   }
+  //   const page = parseInt(req.query.page, 10) || 1
+  //   const limit = parseInt(req.query.limit, 10) || 24
+  //   const startIndex = (page - 1) * limit
+  //   const endIndex = page * limit
+  //   const total = await model.countDocuments()
+  //   query = query.skip(startIndex).limit(limit)
+  //   if (populate) {
+  //     query = query.populate(populate)
+  //   }
+  //   interface IPagination {
+  //     next: {}
+  //     previous: {}
+  //   }
+  //   const results = await query
+  //   interface IPaginition {
+  //     prev: any
+  //     next: any
+  //   }
+  //   const pagination: IPaginition = {
+  //     prev: 0,
+  //     next: 0,
+  //   }
+  //   if (endIndex < total) {
+  //     pagination.next = {
+  //       page: page + 1,
+  //       limit,
+  //     }
+  //   }
+  //   if (startIndex > 0) {
+  //     pagination.prev = {
+  //       page: page - 1,
+  //       limit,
+  //     }
+  //   }
+  //   res.advancedQuery = {
+  //     success: true,
+  //     count: results.length,
+  //     pagination,
+  //     data: results,
+  //   }
+  //   next()
 }
 
 export default advancedQuery
