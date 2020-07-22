81c3dabdedbcdb47b5956560d58604648484ebaf
Samsoedien <contact@samsoedien.com>
2020-07-23 00:13:31 +0200

    feat: isolate input logic into separate component and adapt props to formik api

--- packages/client/src/App/features/Contact/ContactFormik/ContactFormik.tsx
+++ packages/client/src/App/features/Contact/ContactFormik/ContactFormik.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 // import PropTypes from 'prop-types'
 import { Formik } from 'formik'
 
-import Input from './Input'
+import Input from '../../../components/Input'
 import { ContactSchema } from './contactFormValidation'
 
 export interface IContactFormik {}
@@ -23,16 +23,16 @@ const ContactFormik: React.FC<IContactFormik> = () => {
       {({ handleSubmit, handleChange, isSubmitting, values, dirty, touched, errors }) => (
         <form onSubmit={handleSubmit}>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" value={values.name} onChange={handleChange} className="form-control" />
-          <span className="form-text text-muted">{touched && errors.name}</span>
+          <Input name="name" value={values.name} onChange={handleChange} />
+          <span className="form-text text-muted">{touched.name && errors.name}</span>
 
           <label htmlFor="email">Email Address</label>
-          <input type="text" name="email" value={values.email} onChange={handleChange} className="form-control" />
-          <span className="form-text text-muted">{touched && errors.email}</span>
+          <Input name="email" value={values.email} onChange={handleChange} />
+          <span className="form-text text-muted">{touched.email && errors.email}</span>
 
           <label htmlFor="subject">Subject</label>
-          <input type="text" name="subject" value={values.subject} onChange={handleChange} className="form-control" />
-          <span className="form-text text-muted">{touched && errors.subject}</span>
+          <Input name="subject" value={values.subject} onChange={handleChange} />
+          <span className="form-text text-muted">{touched.subject && errors.subject}</span>
 
           <label htmlFor="message">Message</label>
           <textarea
@@ -42,7 +42,7 @@ const ContactFormik: React.FC<IContactFormik> = () => {
             onChange={handleChange}
             className="form-control"
           ></textarea>
-          <span className="form-text text-muted">{touched && errors.message}</span>
+          <span className="form-text text-muted">{touched.message && errors.message}</span>
 
           <button type="submit" className="btn btn-primary" disabled={isSubmitting || !dirty}>
             {isSubmitting ? 'Loading...' : 'Submit'}
